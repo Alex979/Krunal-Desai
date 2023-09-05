1948e3eff37a617b75b46744e6bc2226052b2486
Alex Broaddus <alexbroaddus1@gmail.com>
2023-09-04 19:13:02 -0700

    Adding hero image to gallery page

--- app/gallery/[slug]/page.tsx
+++ app/gallery/[slug]/page.tsx
@@ -3,6 +3,8 @@ import Navbar from "@/components/Navbar";
 import { getLocation } from "@/lib/locations";
 import { getPhotos } from "@/lib/photos";
 import NetlifyImage from "@/components/NetlifyImage";
+import lion from "../../lion.jpg";
+import Image from "next/image";
 
 export default async function Gallery({
   params,
@@ -29,9 +31,21 @@ export default async function Gallery({
 
   return (
     <main>
-      <Navbar theme="dark" />
-      <div className="container mx-auto px-5 sm:px-10 md:px-16 lg:px-24 xl:px-32 pt-32 text-center text-slate-800">
-        <h1 className="text-5xl mb-20">{locationData.title}</h1>
+      <div className="relative w-full h-[50vh]">
+        <Image
+          className="absolute inset-0 -z-10"
+          src={lion}
+          alt="lion"
+          fill
+          style={{ objectFit: "cover" }}
+          placeholder="blur"
+          priority
+          sizes="(max-aspect-ratio: 3/4) 75vh, 100vw"
+        />
+        <Navbar theme="light" />
+      </div>
+      <div className="container mx-auto px-5 sm:px-10 md:px-16 lg:px-24 xl:px-32 text-center text-slate-800">
+        <h1 className="text-4xl my-20 leading-normal">{locationData.title}</h1>
         <div className="grid gap-2 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
           {photos}
         </div>
