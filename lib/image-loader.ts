aebe458191d67f49e6934210c4a7a0334d154cd8
Alex Broaddus <alexbroaddus1@gmail.com>
2023-09-11 11:08:26 -0700

    Adding fit and drop parameters to thumbnails

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -12,7 +12,7 @@ export default function imgixImageLoader(square?: boolean) {
 
 function imgixImageLoaderInternal(src: string, width: number, height?: number | undefined): string {
   if (height !== undefined) {
-    return `https://krunal-desai.imgix.net/${src}?w=${width}&h=${height}`;
+    return `https://krunal-desai.imgix.net/${src}?w=${width}&h=${height}&fit=crop&crop=edges`;
   } else {
     return `https://krunal-desai.imgix.net/${src}?w=${width}`;
   }
