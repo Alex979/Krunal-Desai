56ae9c00736faad281731684f1dabe71462a862a
Alex Broaddus <alexbroaddus1@gmail.com>
2023-10-07 20:49:02 -0700

    Adding links to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,17 +9,30 @@ interface NavbarProps {
 
 export default function Navbar({ theme }: NavbarProps) {
   return (
-    <div className="absolute w-full h-20 flex">
+    <div className="absolute w-full flex h-16 sm:h-20 z-10">
       <div className="flex items-center">
         <Link href="/">
           <Image
-            className="mx-5 w-64"
+            className="mx-2 w-44 sm:mx-5 sm:w-64"
             src={theme === "light" ? signatureLight : signatureDark}
             alt="signature"
           />
         </Link>
       </div>
-      <div className="grow">{/* Put nav links here */}</div>
+      <div className="grow px-5 flex items-center justify-end space-x-6">
+        <Link
+          href="/map"
+          className="text-white hover:text-gray-300 transition text-lg"
+        >
+          Map
+        </Link>
+        <Link
+          href="/gallery"
+          className="text-white hover:text-gray-300 transition text-lg"
+        >
+          Gallery
+        </Link>
+      </div>
     </div>
   );
 }
