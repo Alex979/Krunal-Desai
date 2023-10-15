36ffc04b76187e2dc4e95605cc15b359b39e40ec
Alex Broaddus <alexbroaddus1@gmail.com>
2023-10-15 10:52:39 -0700

    Making image optional and giving the option to change the background overlay

--- components/ImageSection.tsx
+++ components/ImageSection.tsx
@@ -5,7 +5,8 @@ import { ReactNode } from "react";
 
 interface ImageSectionProps {
   children?: ReactNode;
-  image: Media;
+  image?: Media;
+  bgClassName?: string;
   priority?: boolean;
   halfHeight?: boolean;
   navbar?: boolean;
@@ -15,32 +16,42 @@ interface ImageSectionProps {
 export default function ImageSection({
   children,
   image,
+  bgClassName = "bg-black opacity-20",
   priority,
   halfHeight,
   navbar,
   noTopPadding,
 }: ImageSectionProps) {
   return (
-    <div className={"w-full " + (halfHeight ? "h-[50vh] " : "h-screen ") + (noTopPadding ? "px-3 pb-3" : "p-3")}>
+    <div
+      className={
+        "w-full " +
+        (halfHeight ? "h-[50vh] " : "h-screen ") +
+        (noTopPadding ? "px-3 pb-3" : "p-3")
+      }
+    >
       <div className="w-full h-full relative">
         {navbar && <Navbar theme="light" />}
-        <Imgix
-          className="absolute inset-0 select-none"
-          src={image.filename!}
-          alt={image.alt}
-          fill
-          style={{ objectFit: "cover" }}
-          placeholder="blur"
-          blurDataURL={image.blurDataUrl}
-          priority={priority}
-          sizes={`(max-aspect-ratio: ${image.width!}/${
-            halfHeight ? image.height! * 2 : image.height!
-          }) ${Math.floor(
-            (image.width! / (halfHeight ? image.height! * 2 : image.height!)) *
-              100
-          )}vh, 100vw`}
-        />
-        <div className="absolute inset-0 bg-black opacity-20"></div>
+        {image && (
+          <Imgix
+            className="absolute inset-0 select-none"
+            src={image.filename!}
+            alt={image.alt}
+            fill
+            style={{ objectFit: "cover" }}
+            placeholder="blur"
+            blurDataURL={image.blurDataUrl}
+            priority={priority}
+            sizes={`(max-aspect-ratio: ${image.width!}/${
+              halfHeight ? image.height! * 2 : image.height!
+            }) ${Math.floor(
+              (image.width! /
+                (halfHeight ? image.height! * 2 : image.height!)) *
+                100
+            )}vh, 100vw`}
+          />
+        )}
+        <div className={"absolute inset-0 " + bgClassName}></div>
         <div className="absolute w-full h-full">{children}</div>
       </div>
     </div>
