a4c23912dcce05f8de2fe05efc3c153b2dde7155
Alex Broaddus <alexbroaddus1@gmail.com>
2023-11-16 18:57:49 -0800

    Adding fancy dividers to biography page

--- app/biography/page.tsx
+++ app/biography/page.tsx
@@ -1,5 +1,8 @@
 import ImageSection from "@/components/ImageSection";
 import { getBiographyPage } from "@/lib/globals";
+import fancyDividerDark from "@/images/fancy-divider-dark.png";
+import fancyDividerLight from "@/images/fancy-divider-light.png";
+import Image from "next/image";
 
 export default async function BiographyPage() {
   const biographyPage = await getBiographyPage();
@@ -19,25 +22,50 @@ export default async function BiographyPage() {
         priority
         navbar
       ></ImageSection>
-      <ImageSection noTopPadding bgClassName="bg-stone-100">
+      <ImageSection variableHeightOnMobile noTopPadding bgClassName="bg-stone-100">
         <div className="w-full h-full flex justify-center items-center">
-          <div className="w-full max-w-3xl p-8">
-            <p className="text-lg md:text-2xl text-slate-600 leading-normal">
+          <div className="w-full max-w-3xl p-8 space-y-8">
+            <Image
+              src={fancyDividerDark}
+              width={200}
+              alt="fancy divider"
+              className="mx-auto"
+            />
+            <p className="text-lg md:text-2xl text-slate-600 leading-normal text-center">
               {biographyPage.biographyText}
             </p>
+            <Image
+              src={fancyDividerDark}
+              width={200}
+              alt="fancy divider"
+              className="mx-auto rotate-180"
+            />
           </div>
         </div>
       </ImageSection>
       <ImageSection
         noTopPadding
+        variableHeightOnMobile
         image={biographyPage.conservationSection.image}
         bgClassName="bg-black opacity-40"
       >
         <div className="w-full h-full flex justify-center items-center">
-          <div className="w-full max-w-3xl p-8">
-            <p className="text-lg md:text-2xl text-white leading-normal">
+          <div className="w-full max-w-3xl p-8 space-y-8">
+            <Image
+              src={fancyDividerLight}
+              width={200}
+              alt="fancy divider"
+              className="mx-auto"
+            />
+            <p className="text-lg md:text-2xl text-white leading-normal text-center">
               {biographyPage.conservationSection.text}
             </p>
+            <Image
+              src={fancyDividerLight}
+              width={200}
+              alt="fancy divider"
+              className="mx-auto rotate-180"
+            />
           </div>
         </div>
       </ImageSection>
