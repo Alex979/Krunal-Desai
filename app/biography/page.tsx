6f62675a939fca57d378c8018171a5557a30d615
Alex Broaddus <alexbroaddus1@gmail.com>
2024-01-12 08:30:25 -0800

    Changing fancy divider

--- app/biography/page.tsx
+++ app/biography/page.tsx
@@ -2,6 +2,7 @@ import ImageSection from "@/components/ImageSection";
 import { getBiographyPage } from "@/lib/globals";
 import fancyDividerDark from "@/images/fancy-divider-dark.png";
 import fancyDividerLight from "@/images/fancy-divider-light.png";
+import featherIconDark from "@/images/feather-icon-dark.png";
 import Image from "next/image";
 
 export default async function BiographyPage() {
@@ -26,20 +27,14 @@ export default async function BiographyPage() {
         <div className="w-full h-full flex justify-center items-center">
           <div className="w-full max-w-3xl p-8 space-y-8">
             <Image
-              src={fancyDividerDark}
-              width={200}
-              alt="fancy divider"
+              src={featherIconDark}
+              width={150}
+              alt="feather icon"
               className="mx-auto"
             />
             <p className="text-lg md:text-2xl text-slate-600 leading-normal text-center">
               {biographyPage.biographyText}
             </p>
-            <Image
-              src={fancyDividerDark}
-              width={200}
-              alt="fancy divider"
-              className="mx-auto rotate-180"
-            />
           </div>
         </div>
       </ImageSection>
