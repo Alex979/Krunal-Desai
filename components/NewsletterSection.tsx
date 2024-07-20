9d14be5cb6f2bf2e9aff73a340316749069fa175
Alex Broaddus <alexbroaddus1@gmail.com>
2024-07-20 13:45:03 -0700

    Fixing newsletter section overlapping with photo

--- components/NewsletterSection.tsx
+++ components/NewsletterSection.tsx
@@ -11,33 +11,36 @@ export default async function NewsletterSection() {
   }
 
   return (
-    <ImageSection
-      image={newsletter.backgroundImage}
-      noTopPadding
-      bgClassName=""
-      variableHeightOnMobile
-      removeBgOnMobile
-    >
-      <div className="w-full flex items-center justify-center flex-col py-10" data-aos="fade">
-        <h1 className="text-4xl md:text-5xl my-6 md:my-10 md:text-white font-bold text-center">
-          Join My Newsletter
-        </h1>
-        <NewsletterForm />
+    <>
+      <div className="md:px-3">
+        <div className="w-full flex items-center justify-center flex-col pt-10 bg-[#87a5e3]">
+          <h1 className="text-4xl md:text-5xl my-6 md:my-10 text-white font-bold text-center">
+            Join My Newsletter
+          </h1>
+          <NewsletterForm />
+        </div>
       </div>
-      <div className="w-full aspect-[3/2] relative md:hidden">
-        <Imgix
-          className=""
-          src={newsletter.backgroundImage.filename!}
-          alt={newsletter.backgroundImage.alt}
-          fill
-          style={{ objectFit: "cover" }}
-          placeholder="blur"
-          blurDataURL={
-            newsletter.backgroundImage.blurDataUrl || undefined
-          }
-          sizes="100vw"
-        />
-      </div>
-    </ImageSection>
+      <ImageSection
+        image={newsletter.backgroundImage}
+        noTopPadding
+        bgClassName="bg-gradient-to-b from-[#87a5e3] to-transparent to-10%"
+        removeBgOnMobile
+        variableHeightOnMobile
+      >
+        <div className="w-full aspect-[3/2] relative md:hidden">
+          <Imgix
+            className=""
+            src={newsletter.backgroundImage.filename!}
+            alt={newsletter.backgroundImage.alt}
+            fill
+            style={{ objectFit: "cover" }}
+            placeholder="blur"
+            blurDataURL={newsletter.backgroundImage.blurDataUrl || undefined}
+            sizes="100vw"
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-[#87a5e3] to-transparent to-10%"></div>
+        </div>
+      </ImageSection>
+    </>
   );
 }
