049359c9064ee88c1304b26377fc35a931f7fd4e
Alex Broaddus <alexbroaddus1@gmail.com>
2024-07-28 16:01:08 -0700

    Setting object-position of falcon photo to ensure text stays visible

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -136,6 +136,7 @@ export default async function Home() {
           bgClassName=""
           variableHeightOnMobile
           removeBgOnMobile
+          objectPosition="object-[center_10%]"
         >
           <div
             className={
